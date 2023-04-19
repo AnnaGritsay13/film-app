1f955a469b1553a9c0c713ee8ca54e449c65ec44
AnnaGritsay13 <agritsay_1999@mail.ru>
2023-04-19 14:56:46 +0300

    removed import react

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
 
 import './movie.css'
--- src/components/renderMovie/renderMovie.js
+++ src/components/renderMovie/renderMovie.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Movie from "../movie/movie";
 
 import '../../App.css'
